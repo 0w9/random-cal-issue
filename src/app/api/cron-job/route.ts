bb7df853468bf96fdbfa55a9e2e83b7d0850086b
Lennard Dorst <lennardsolana@gmail.com>
2024-11-30 00:52:51 +0100

    Adding keys

--- src/app/api/cron-job/route.ts
+++ src/app/api/cron-job/route.ts
@@ -46,7 +46,7 @@ export async function GET() {
             );
             const result = await updateEdgeConfig.json();
             console.log(result);
-        } 
+        
 
             return NextResponse.json(randomIssue);
         } else {
